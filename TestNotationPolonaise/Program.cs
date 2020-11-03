d3f3aeb7bbcbc78a5f2220e662ab941bd33bd33d
groli <groli@LAPTOP-0M7TN1RD>
2020-11-03 01:05:02 +0100

    fonction pour la notation polonaise

--- TestNotationPolonaise/Program.cs
+++ TestNotationPolonaise/Program.cs
@@ -4,6 +4,9 @@
  * date : 20/06/2020
  */
 using System;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq.Expressions;
 
 namespace TestNotationPolonaise
 {
@@ -27,7 +30,75 @@ namespace TestNotationPolonaise
             } while (reponse != carac1 && reponse != carac2);
             return reponse;
         }
+        /// <summary>
+        /// Permet de calculer à partir de la notation polonaise
+        /// </summary>
+        /// <param name="laFormule"></param>
+        /// <param name="resultat"></param>
+        /// <returns>le resultat du calcul en notation polonaise </returns>
 
+        static Double Polonaise(String formule)
+        {
+
+            try
+            {
+                // transformation de la formule en vecteur
+                string[] vec = formule.Split(' ');
+                int nbCases = vec.Length;
+
+                // boucle tantqu'il ne reste pas q'une seule case
+                while (nbCases > 1)
+                {
+                    // recherche d'un signe à partir de la fin
+                    int k = nbCases - 1;
+                    while (k > 0 && vec[k] != "+" && vec[k] != "-" && vec[k] != "*" && vec[k] != "/")
+                    {
+                        k--;
+                    }
+                    // récupération des 2 valeurs concernées par le calcul
+                    float a = float.Parse(vec[k + 1]);
+                    float b = float.Parse(vec[k + 2]);
+
+                    // calcul
+                    float result = 0;
+                    switch (vec[k])
+                    {
+                        case "+": result = a + b; break;
+                        case "-": result = a - b; break;
+                        case "*": result = a * b; break;
+                        case "/":
+                            // éviter la division par 0
+                            if (b == 0)
+                            {
+                                return Double.NaN;
+                            }
+                            result = a / b; break;
+                    }
+                    // stockage du résultat à la place du signe
+                    vec[k] = result.ToString();
+
+                    // suppression des 2 cellules suivantes par décalage vers la gauche
+                    for (int j = k + 1; j < nbCases - 2; j++)
+                    {
+                        vec[j] = vec[j + 2];
+                    }
+                    // les cases suivantes sont mises à blanc
+                    for (int j = nbCases - 2; j < nbCases; j++)
+                    {
+                        vec[j] = " ";
+
+                    }
+                    nbCases = nbCases - 2;
+                }
+                // retour du résultat final
+                return Double.Parse(vec[0]);
+            }
+            catch
+            {
+                // erreur rencontrée
+                return Double.NaN;
+            }
+        }
         /// <summary>
         /// Saisie de formules en notation polonaise pour tester la fonction de calcul
         /// </summary>
